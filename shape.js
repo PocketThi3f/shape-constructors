227564601b2f52e72f08d940d0989d0f6c3cd43a
PocketThi3f <krslai@yahoo.com>
2016-12-07 18:45:50 -0800

    shapingup

--- shape.js
+++ shape.js
@@ -1,1 +1,54 @@
+function Shape() {
 
+	this.get_type = function () {
+
+		return;
+	}
+}
+
+function Square() {
+
+	this.side1
+	this.side2
+	this.side3
+}
+
+function Square() {
+
+	this.side1
+	this.side2
+	this.side3
+}
+
+var Polygon = 
+
+Triangle.prototype = new Shape();
+
+//	Reference: Shapes Construct
+//================================================
+// 'use strict';
+
+// var Shape = require('./Shape.js'),
+//     Triangle = require('./Triangle.js'),
+//     Square = require('./Square.js'),
+//     Pentagon = require('./Pentagon.js');
+
+// // Let's give it a whirl...
+// var triangle = new Triangle(1, 2, 3);
+// var square = new Square(4);
+// var pentagon = new Pentagon(1, 2, 3, 4, 5);
+// var shape = new Shape();
+
+// // Ensuring everything delegates to Shape...
+// console.log(triangle.get_type());
+// console.log(square.get_type());
+// console.log(pentagon.get_type());
+// console.log(shape);
+
+// console.log('-'.repeat(24));
+
+// // Ensuring instanceof works as expected.
+// console.log(triangle instanceof Triangle);
+// console.log(square instanceof Square);
+// console.log(pentagon instanceof Pentagon);
+// console.log(shape instanceof Shape);
